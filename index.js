3d04596c283f0f3f06b3148c0414ed6bd29fb4ac
Emmanuel-FSE <emmanuelmutisya254@gmail.com>
2022-11-24 19:04:52 +0300

    return a function

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function (drivers){
+    const r1 = drivers.slice(0,2);
+    return r1;
+}
+
+const returnLastTwoDrivers = function (drivers){
+    const r2 =  drivers.slice(-2);
+    return r2;
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(int){
+    return function exactFare (fare){
+        return fare * int;
+    }
+}
+const fareDoubler = createFareMultiplier(int)
